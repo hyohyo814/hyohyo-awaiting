d68555172d989d749489c1dc990b9f0c6278ccfc
hyohyo814 <sojaisam@gmail.com>
2023-08-28 17:35:50 -0700

    Checkpoint in matching algorithm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,18 +8,65 @@ export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
   const { user, isSignedIn, isLoaded } = useUser();
 
+  const test = "Did you know that the critically acclaimed MMORPG Final Fantasy XIV has a free trial, and includes the entirety of A Realm Reborn AND the award-winning Heavensward and Stormblood expansions up to level 70 with n restrictions on playtime? Sign up, and enjoy Eorzea today!"
+  const testArr = test.split("");
+  let group = 0;
+  const testMap = testArr.map((el, idx) => {
+    if (el === " ") {
+      group++
+      return <span key={idx} className="word_spacer">{el}</span>
+    }
+    return <span id={`${group}`} key={idx} className="">{el}</span>
+  });
+
+  console.log(testMap)
+ 
+
+  function typingHandle(e: React.SyntheticEvent) {
+    e.preventDefault();
+    const target = e.target as HTMLInputElement;
+    const inputSplit = target.value.split("");
+    const displayChars = document.getElementsByClassName("display_char") as HTMLCollectionOf<HTMLSpanElement>;
+    console.log(inputSplit)
+    inputSplit.forEach((el, idx) => {
+      if (el === displayChars[idx]?.innerText) {
+        console.log("match")
+      } else {
+        console.log("wrong")
+      }
+    })
+    
+  }
+
   return (
     <PageLayout>
-      <h1 className="front_title text-5xl font-semibold tracking-tight text-white sm:text-[5rem]">
-        Speed<span className="text-rose-600">pasta</span>
-      </h1>
       {!isSignedIn &&
-        <div className="get_started flex flex-col gap-12 text-white text-3xl font-light items-center">
-          <p>Test out your ability to churn out copypastas</p>
-          <SignInButton>
-            <button className="get_started w-64 h-28 bg-rose-500 rounded-3xl">Get Started</button>
-          </SignInButton>
-        </div>}
+        <>
+          <h1 className="front_title text-5xl font-semibold tracking-tight text-white sm:text-[5rem]">
+            Speed<span className="text-rose-600">pasta</span>
+          </h1>
+          <div className="get_started flex flex-col gap-12 text-white text-3xl font-light items-center">
+            <p>Test out your ability to churn out copypastas</p>
+            <SignInButton>
+              <button className="get_started w-64 h-28 bg-rose-500 rounded-3xl">Get Started</button>
+            </SignInButton>
+          </div>
+        </>}
+      {!!isSignedIn &&
+        <>
+          <div className="flex flex-col bg-slate-900 w-[64rem] h-[40rem]">
+            <input
+              id="input_display"
+              className="w-full text-white break-normal text-4xl bg-slate-800 z-50 px-12 p-1
+              font-light tracking-tight"
+              onChange={typingHandle}
+            />
+            <div id="text_display" className="w-full text-white break-normal text-4xl px-12 py-2 tracking-tight font-light">
+              {testMap}
+            </div>
+          </div>
+        </>
+      }
     </PageLayout>
   );
 }
