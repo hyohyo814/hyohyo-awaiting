2e48f0dfe7f7739b65d169872b5e254e3ce6fd58
hyohyo814 <sojaisam@gmail.com>
2023-09-07 16:15:16 -0700

    schema addition: user records

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { InferModel, sql } from "drizzle-orm";
+import { InferModel, sql, relations } from "drizzle-orm";
 import { integer, sqliteTable, text, uniqueIndex } from "drizzle-orm/sqlite-core";
 
 export const users = sqliteTable("users", {
@@ -7,6 +7,10 @@ export const users = sqliteTable("users", {
   createdAt: integer("created_at").default(sql`(cast (unixepoch () as int))`),
   updatedAt: integer("created_at").default(sql`(cast (unixepoch () as int))`),
 })
+
+export const usersRelations = relations(users, ({ many }) => ({
+  records: many(records),
+}));
 
 export type User = InferModel<typeof users>;
 
@@ -18,3 +22,16 @@ export const codes = sqliteTable("codes", {
 })
 
 export type Code = InferModel<typeof codes>;
+
+export const records = sqliteTable("records", {
+  userId: text("userId"),
+  time: integer("time"),
+  createdAt: integer("created_at").default(sql`(cast (unixepoch () as int))`),
+})
+
+export const recordsRelations = relations(records, ({ one }) => ({
+  user: one(users, {
+    fields: [records.userId],
+    references: [users.id],
+  }),
+}));
--- src/server/api/routers/users.ts
+++ src/server/api/routers/users.ts
@@ -1,10 +1,21 @@
-// import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { users } from "drizzle/schema";
 
 export const userRouter = createTRPCRouter({
   getUsers: publicProcedure.query(async ({ ctx }) => {
     return await ctx.db.select().from(users).all(); 
-  })
+  }),
+
+  saveData: privateProcedure
+  .input(
+    z.object({
+      time: z.number(),
+    })
+  )
+  .query(async ({ ctx, input }) => {
+    
+  }) 
+
 });
 
